90fe90513510e6f74170a54b1f906e2b813b6ad9
OdeiOtxoaErrrarte <ode_otx_flo@goierrieskola.org>
2026-02-04 09:09:16 +0100

    Produktuak.php bakarrik jarri ditut filtroa erabiltzean ikusteko produktuak.

--- Produktuak.php
+++ Produktuak.php
@@ -10,86 +10,12 @@
   <body>
     <?php
     include "Menua.html";
-    include "filtro.php";
     ?>
     <div class="eduki_nagusia">
-  
-  <main class="main-produktuak">
-    <main>
-      <section class="SekzioarenTitulua">
-        <div class="SekzioTitulua"><h2>ORDENAGAILUAK</h2></div>
-      </section>
-      <section class="Ordenagailuak">
-        <div class="ProduktuZerrenda">
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_1.jpg" width="80%" height="80%" /><h3>DELL OPTIPLEX 7010</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_2.jpg" width="80%" height="80%" /><h3>LENOVO THINKPAD E15</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_3.jpg" width="80%" height="80%" /><h3>ASUS TUF F15</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_4.jpg" width="80%" height="80%" /><h3>INTEL NUC 11</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_5.jpg" width="80%" height="80%" /><h3>APPLE IMAC 24</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_6.webp" width="80%" height="80%" /><h3>DELL POWEREDGE T40</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/ordenagailu_7.jpg" width="80%" height="80%" /><h3>ASUS VIVOBOOK 15</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-        </div>
-      </section>
-
-      <section class="SekzioarenTitulua">
-        <div class="SekzioTitulua"><h2>KAMARAK</h2></div>
-      </section>
-      <section class="Kamarak">
-        <div class="ProduktuZerrenda">
-          <div class="Produktuak"><img src="Argazkiak/kamara_1.avif" width="80%" height="80%" /><h3>CANON EOS 250D</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_2.jpg" width="80%" height="80%" /><h3>SONY ALPHA A6400</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_3.webp" width="80%" height="80%" /><h3>ALEXA ARRI MINI</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_4.webp" width="80%" height="80%" /><h3>GOPRO HERO 11</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_5.webp" width="80%" height="80%" /><h3>PANASONIC LUMIX FZ82</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_6.webp" width="80%" height="80%" /><h3>FUJIFILM INSTAX MINI 12</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kamara_7.webp" width="80%" height="80%" /><h3>NIKON Z6 II</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-        </div>
-      </section>
-
-      <section class="SekzioarenTitulua">
-        <div class="SekzioTitulua"><h2>KONTSOLAK</h2></div>
-      </section>
-      <section class="Kontsolak">
-        <div class="ProduktuZerrenda">
-          <div class="Produktuak"><img src="Argazkiak/kontsola_1.webp" width="80%" height="80%" /><h3>SONY PLAYSTATION 5</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_2.webp" width="80%" height="80%" /><h3>MICROSOFT XBOX SERIES X</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_3.jpg" width="80%" height="80%" /><h3>NINTENDO SWITCH OLED</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_4.webp" width="80%" height="80%" /><h3>SEGA MEGA DRIVE MINI</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_5.png" width="80%" height="80%" /><h3>VALVE STEAM DECK</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_6.jpg" width="80%" height="80%" /><h3>SONY PLAYSTATION 4 SLIM</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/kontsola_7.webp" width="80%" height="80%" /><h3>MICROSOFT XBOX SERIES S</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-        </div>
-      </section>
-
-      <section class="SekzioarenTitulua">
-        <div class="SekzioTitulua"><h2>AUDIOBISUAL PERIFERIKOAK</h2></div>
-      </section>
-      <section class="Periferikoak">
-        <div class="ProduktuZerrenda">
-          <div class="Produktuak"><img src="Argazkiak/periferiko_1.webp" height="80%" /><h3>SONY WH-1000XM5</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_2.webp" width="80%" height="80%" /><h3>JBL CHARGE 5</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_3.webp" width="80%" height="80%" /><h3>LOGITECH BLUE YETI</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_4.webp" width="80%" height="80%" /><h3>SAMSUNG HW-Q800B</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_5.jpg" width="80%" height="80%" /><h3>LOGITECH C920 HD PRO</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_6.webp" width="80%" height="80%" /><h3>HYPERX CLOUD II</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/periferiko_7.webp" width="80%" height="80%" /><h3>DELL ULTRASHARP U2720Q</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-        </div>
-      </section>
-
-      <section class="SekzioarenTitulua">
-        <div class="SekzioTitulua"><h2>TEKLATUAK</h2></div>9
-      </section>
-      <section class="Teklatuak">
-        <div class="ProduktuZerrenda">
-          <div class="Produktuak"><img src="Argazkiak/teklatu_1.webp" width="80%" height="80%" /><h3>CORSAIR K95 RGB</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_2.webp" width="80%" height="80%" /><h3>RAZER BLACKWIDOW V3</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_3.webp" width="80%" height="80%" /><h3>LOGITECH MX KEYS</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_4.jpg" width="80%" height="80%" /><h3>OBINS ANNE PRO 2</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_5.webp" width="80%" height="80%" /><h3>LOGITECH K860</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_6.jpg" width="80%" height="80%" /><h3>DUCKY ONE 3</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-          <div class="Produktuak"><img src="Argazkiak/teklatu_7.webp" width="80%" height="80%" /><h3>LOGITECH K120</h3><button class="ErosiBotoia"><strong>GEHITU SASKIRA</strong></button></div>
-        </div>
-      </section>
-    </main>
+  <sidebar>
+    <?php
+    include "filtro.php";
+    ?>
+  </sidebar>
   </body>
 </html>
