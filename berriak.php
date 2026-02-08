76807b9d812f84081ed80f4420e4d533ae4c6075
Kimetz Milan <kim_mil_apa@goierrieskola.org>
2026-02-08 16:30:58 +0100

    berriak sortzen lehenengo commit

---
+++ berriak.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="eu">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>BirtekIndex</title>
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+</head>
+
+<body>
+
+  <?php include "Menua.php";?>
+
+<div class="container">
+
+    <div class="berria">
+        <img src="Argazkiak\Foto-IA.jpg" alt="AA irudia">
+        <div class="edukia">
+            <div class="titulua">Adimen Artifizialaren erabilera gure enpresan</div>
+            <div class="data">2026-02-08</div>
+            <p>AA sistemek azken hilabeteetan hobekuntza handiak izan dituzte, eta beraien erabilera gure enpresan igo da.</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\movilberria.jpg" alt="Mugikor berriak">
+        <div class="edukia">
+            <div class="titulua">Mugikor belaunaldi berria iritsi da</div>
+            <div class="data">2026-02-05</div>
+            <p>5G+ teknologia eta bateria autonomia handiagoa izango dituzte gailu berriek, gure enpresan saltzen hasi gara.</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\ziberseguridadberriak.webp" alt="Zibersegurtasuna">
+        <div class="edukia">
+            <div class="titulua">Zibersegurtasun arriskuak gora egiten</div>
+            <div class="data">2026-02-01</div>
+            <p>Adituek ohartarazi dute datuen babesa funtsezkoa dela, eta neurri zorrotzagoak hartzen hasi gara.</p>
+        </div>
+    </div>
+
+</div>
+<div class="container">
+
+    <div class="berria">
+        <img src="Argazkiak\Foto-IA.jpg" alt="AA irudia">
+        <div class="edukia">
+            <div class="titulua">Adimen Artifizialaren erabilera gure enpresan</div>
+            <div class="data">2026-02-08</div>
+            <p>AA sistemek azken hilabeteetan hobekuntza handiak izan dituzte, eta beraien erabilera gure enpresan igo da..</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\movilberria.jpg" alt="Mugikor berriak">
+        <div class="edukia">
+            <div class="titulua">Mugikor belaunaldi berria iritsi da</div>
+            <div class="data">2026-02-05</div>
+            <p>5G+ teknologia eta bateria autonomia handiagoa izango dituzte gailu berriek, gure enpresan saltzen hasi gara.</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\ziberseguridadberriak.webp" alt="Zibersegurtasuna">
+        <div class="edukia">
+            <div class="titulua">Zibersegurtasun arriskuak gora egiten</div>
+            <div class="data">2026-02-01</div>
+            <p>Adituek ohartarazi dute datuen babesa funtsezkoa dela,  eta neurri zorrotzagoak hartzen hasi gara.</p>
+        </div>
+    </div>
+
+</div>
+<div class="container">
+
+    <div class="berria">
+        <img src="Argazkiak\Foto-IA.jpg" alt="AA irudia">
+        <div class="edukia">
+            <div class="titulua">Adimen Artifizialaren erabilera gure enpresan</div>
+            <div class="data">2026-02-08</div>
+            <p>AA sistemek azken hilabeteetan hobekuntza handiak izan dituzte, eta beraien erabilera gure enpresan igo da.</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\movilberria.jpg" alt="Mugikor berriak">
+        <div class="edukia">
+            <div class="titulua">Mugikor belaunaldi berria iritsi da</div>
+            <div class="data">2026-02-05</div>
+            <p>5G+ teknologia eta bateria autonomia handiagoa izango dituzte gailu berriek, gure enpresan saltzen hasi gara.</p>
+        </div>
+    </div>
+
+    <div class="berria">
+        <img src="Argazkiak\ziberseguridadberriak.webp" alt="Zibersegurtasuna">
+        <div class="edukia">
+            <div class="titulua">Zibersegurtasun arriskuak gora egiten</div>
+            <div class="data">2026-02-01</div>
+            <p>Adituek ohartarazi dute datuen babesa funtsezkoa dela, eta neurri zorrotzagoak hartzen hasi gara.</p>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
